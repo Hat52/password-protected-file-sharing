e7140674f607d5b7db8696ba75394f6b07cde422
Hat52 <tanolihamzaali@gmail.com>
2022-07-08 21:47:55 +0500

    setup multer

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
+const multer = require('multer')
 const express = require('express');
 const app = express();
 app.use("/static",express.static(__dirname + "/styles"));
 app.set('view engine', 'ejs')
+const upload = multer({ dest: '/uploads/' })
 app.get('/',(req,res)=>{
     res.render("index")
 })
 
-app.post('/upload',(req,res)=>{
+app.post('/upload',upload.single('uploaded_file'),(req,res)=>{
     res.send("Hello")
 })
 
