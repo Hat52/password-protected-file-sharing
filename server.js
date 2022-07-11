ab70583e168b17a23b3849dff1114665a1f23c9e
Hat52 <tanolihamzaali@gmail.com>
2022-07-11 16:11:55 +0500

    Db: connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 const multer = require('multer')
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 app.use("/static",express.static(__dirname + "/styles"));
 app.set('view engine', 'ejs')
 const upload = multer({ dest: 'uploads' })
+mongoose.connect(process.env.DATABASE_URL,()=>{
+    console.log('Connected')
+},
+e=>{
+    console.log('No connection')
+}
+)
 app.get('/',(req,res)=>{
     res.render("index")
 })
